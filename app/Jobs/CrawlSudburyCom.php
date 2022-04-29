24cf30c94dd27354958215dfbd7924601c228c1d
Russell Jones <jonesrussell42@gmail.com>
2022-04-29 00:24:05 +0000

    Create crawl sudbury.com job

---
+++ app/Jobs/CrawlSudburyCom.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Symfony\Component\Process\Process;
+
+class CrawlSudburyCom implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $process = new Process(['streetcode-crawler', 'https://www.sudbury.com/police']);
+        $process->run();
+    }
+}
